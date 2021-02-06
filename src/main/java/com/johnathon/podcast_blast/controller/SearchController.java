6436bab95238a8739135a60c688c76eae2ec4720
Johnathon Wood <jaywood128@gmail.com>
2021-02-06 17:59:38 -0500

    Search Controller: @Get full text-search.

---
+++ src/main/java/com/johnathon/podcast_blast/controller/SearchController.java
@@ -0,0 +1,44 @@
+package com.johnathon.podcast_blast.controller;
+
+import net.minidev.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.Serializable;
+
+
+@RestController
+@RequestMapping("/api")
+public class SearchController {
+    private static final String baseURL = "https://listen-api.listennotes.com/api/v2";
+    @Autowired
+    private  WebClient.Builder webClientBuilder;
+    @Autowired
+    private AppSecurityConfig appSecurityConfig;
+
+    public SearchController() {
+    }
+
+    @GetMapping("/full-text-search/{text}")
+    private ResponseEntity<? extends Serializable> fullTextSearch(@PathVariable String text){
+        JSONObject jsonObject = webClientBuilder
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("X-ListenAPI-Key", appSecurityConfig.getApiKey())
+                .build()
+                .get()
+                .uri(baseURL + "/search?sort_by_date=1&type=episode&offset=0&len_min=10&len_max=200&published_before=1580172454000&published_after=0&only_in=title,description,author,audio&language=English&q=" + text)
+                .retrieve()
+                .bodyToMono(JSONObject.class)
+                .block();
+        if (jsonObject != null) {
+            return new ResponseEntity<>(jsonObject, HttpStatus.OK);
+        }
+        return new ResponseEntity<Error>(HttpStatus.NOT_FOUND);
+    }
+
+}
